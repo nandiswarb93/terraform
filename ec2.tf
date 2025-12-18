6bd4b2c79705a78ad83bf0e5034a31741bfa439a
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-12-18 13:17:20 +0530

    terraform basics

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,36 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.27.0"
-    }
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+  description = "Allow all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all"
   }
 }
 
-provider "aws" {
-  # Configuration options
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name      = "HelloWorld"
+    Terraform = "true"
+  }
 }
